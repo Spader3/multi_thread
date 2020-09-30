0cd955f45779ecc8f259ae068a8729d40ff8e35e
Bronnikov Nikita <spader332@gmail.com>
2020-09-30 15:52:40 +0300

    console app commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var locker = new Object();
+            int count = 0;
+            Parallel.For
+                (0
+                 , 10000
+                 , new ParallelOptions { MaxDegreeOfParallelism = 1 }
+                 , (i) =>
+                 {
+                     Interlocked.Increment(ref count);
+                     lock (locker)
+                     {
+                         Console.WriteLine("Number of active threads:" + count);
+                         Thread.Sleep(10);
+                     }
+                     Interlocked.Decrement(ref count);
+                 }
+                );
+        }
+    }
+}
